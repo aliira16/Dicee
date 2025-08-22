3024b120d8c1d024c1429d4093c82cba9122b26f
aliira16 <116805280+aliira16@users.noreply.github.com>
2025-08-22 09:42:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let imgOne = document.querySelector(".img1");
+let imgTwo = document.querySelector(".img2");
+let h = document.querySelector("h1")
+let btn = document.querySelector("button")
+
+// if (sessionStorage.getItem("visited")) {
+//     getResult()
+// } else {
+//     imgOne.setAttribute("src", `/DOM Challenge Starting Files/images/dice6.png`)
+//     imgTwo.setAttribute("src", `/DOM Challenge Starting Files/images/dice6.png`)
+//     window.sessionStorage.setItem("visited", true)
+// }
+
+
+function getResult() {
+    let numberOne = Math.floor(Math.random() * 6) + 1
+    let numberTwo = Math.floor(Math.random() * 6) + 1
+    imgOne.setAttribute("src", `/DOM Challenge Starting Files/images/dice${numberOne}.png`)
+    imgTwo.setAttribute("src", `/DOM Challenge Starting Files/images/dice${numberTwo}.png`)
+    if (numberOne > numberTwo) {
+        h.textContent = "player 1 win 🎉"
+    } else if (numberTwo > numberOne) {
+        h.textContent = "player 2 win 🎉"
+    } else {
+        h.textContent = "🎲 BIG draw 🎲"
+    }
+}
+
+
+
+btn.onclick = function () {
+    getResult()
+}
+
+
